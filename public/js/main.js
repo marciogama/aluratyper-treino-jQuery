e9525ddcd5570a12fac90543dd9f411e0a8a67cb
marciogama <marciogama2000@gmail.com>
2020-07-16 13:22:30 -0300

    passa a contar num de caracteres e palavras

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
 let frase = $(".frase").text();
 let numPalavras = frase.split(" ").length;
-let tamanhoFrase = $(".tamanho-frase");
+var tamanhoFrase = $("#tamanho-frase")
 
-tamanhoFrase.text(`${numPalavras} palavras`);
+tamanhoFrase.text(numPalavras);
+
+let campo = $(".campo-digitacao");
+campo.on("input",function(){
+    let conteudo = campo.val();
+
+    let qtdPalavras = conteudo.split(/\S+/).length-1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    let qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
+
+
+// .text() pega conteudo de tags <p> <ul> etc.
+// .val() pega conteudo de tags inputs ex.: <textarea> <input> <select>
