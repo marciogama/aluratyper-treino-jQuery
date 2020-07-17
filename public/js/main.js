2f4124293fd7c31967527f5520e4ed7ab72744b3
marciogama <marciogama2000@gmail.com>
2020-07-17 20:17:13 -0300

    icone reinicia jogo e validacao da digitacao

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
 });
 
@@ -28,23 +29,41 @@ function inicializaContadores() {
 };
 
 function inicializaCronometro() {
-    let tempoRestante = $("#tempo-restante").text();
-    campo.one("focus",function(){
+    let tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function(){
         let cronometroID = setInterval(function(){
+            tempoRestante--;
             $("#tempo-digitacao").text(tempoRestante);
-            if(tempoRestante<1){
+            if(tempoRestante < 1){
                 campo.attr("disabled", true);
                 clearInterval(cronometroID);
-                $("#tempo-digitacao").text(0);
+                campo.toggleClass("campo-desativado");
             }
-            tempoRestante--;
-        },1000);
+        }, 1000);
     });
-};
+}
+
+function inicializaMarcadores(){
+    let frase = $(".frase").text();
+    campo.on("input", function(){
+        var digitado = campo.val();
+        var comparavel = frase.substr(0,digitado.length);
+        if(digitado == comparavel){
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+}
 
 function reiniciaJogo() {
     campo.attr("disabled",false);
     campo.val("");
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
     $("#contador-palavras").text("0");
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
