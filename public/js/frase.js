07fb3de456fa849c95f51ec9ef5db63357209294
marciogama <marciogama2000@gmail.com>
2020-08-12 09:41:00 -0300

    trocando frases com uso de Ajax

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
     $("#spinner").toggle();
@@ -12,14 +13,35 @@ function fraseAleatoria() {
     .always(function() {
         $("#spinner").toggle();
     });
-};
+}
 
 function trocaFraseAleatoria(data) {
-    
     let frase = $(".frase");
     let numeroAleatorio = Math.floor(Math.random() * data.length);
     frase.text(data[numeroAleatorio].texto)
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
-    
-};
+}
+
+function buscaFrase(){
+    $("#spinner").toggle();
+    let fraseId = $("#frase-id").val();
+    var dados = { id: fraseId};
+    $.get("http://localhost:3000/frases",dados,trocaFrase)
+    .fail(function() {
+        $("#erro").show();
+        setTimeout(function() {
+            $("#erro").hide();
+        }, 2000);
+    })
+    .always(function() {
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data){
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
+}
